d9cbc102a49d9d96123907d3dbac71bc3491ecd5
rabby <rabby.65301@gmail.com>
2024-07-14 11:27:51 +0600

    added create poduct modal

--- src/pages/Home/HeroSection/HeroSection.tsx
+++ src/pages/Home/HeroSection/HeroSection.tsx
@@ -5,6 +5,8 @@ import slider3 from '../../../assets/images/slider/slider (3).jpg';
 import slider4 from '../../../assets/images/slider/slider (4).jpg';
 
 
+
+
 const HeroSection = () => {
 
     const contentStyle: React.CSSProperties = {
@@ -20,10 +22,10 @@ const HeroSection = () => {
         <>
             <Carousel autoplay >
 
-                <div className=" mx-auto relative">
+                <div className=" relative">
                     <img className="max-h-[600px] w-full " src={slider1} alt="img" />
 
-                    <div className=" text-white absolute top-0 md:top-1/4  lg:top-1/4 xl:top-1/4 lg:bottom-1/4 left-0 md:left-10 lg:left-1/4 xl:left-1/4   p-4  bg-black opacity-75 ">
+                    <div className=" text-white absolute mx-auto   top-0 md:top-1/4  lg:top-1/4 xl:top-1/4 lg:bottom-1/4 left-0 md:left-10 lg:left-1/4 xl:left-1/4   p-4  bg-black opacity-75 ">
                         <div className="text-center">
                             <h1 className="text-4xl uppercase font-bold my-3">Well Come to our Online Nursery</h1>
                             <p className="text-lg "> Brathe better by planting trees</p>
