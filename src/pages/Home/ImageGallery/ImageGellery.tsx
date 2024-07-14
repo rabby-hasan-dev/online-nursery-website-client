676c221abdd98b18692a528a748b487faaefc23e
rabby <rabby.65301@gmail.com>
2024-07-14 22:22:13 +0600

    re design mosic view

--- src/pages/Home/ImageGallery/ImageGellery.tsx
+++ src/pages/Home/ImageGallery/ImageGellery.tsx
@@ -14,32 +14,32 @@ const ImageGellery = () => {
         <div>
 
             <Container>
-                <h1 className='text-4xl font-bold text-center my-[60px] uppercase '>Image Gellery</h1>
-                <div className=" container mx-auto p-4">
-                    <div className="grid grid-cols-3 grid-rows-3 gap-4">
-                        <div className=" col-span-2 row-span-2  ">
-                            <img src={imgGellary1} alt="Image 1" className="object-cover w-full h-full  " />
+                <h1 className='text-4xl font-bold text-center mt-14 mb-10 uppercase '>Image Gellery</h1>
+                <div className=" container mx-auto p-4 ">
+                    <div className="grid grid-cols-3  gap-2 overflow-hidden ">
+                        <div className=" col-span-2 row-span-2 h-[300px]  ">
+                            <img src={imgGellary1} alt="Image 1" className=" w-full h-full   " />
                         </div>
-                        <div className="w-">
-                            <img src={imgGellary2} alt="Image 2" className="object-cover w-full h-full  " />
+                        <div className="h-[150px] ">
+                            <img src={imgGellary2} alt="Image 2" className=" w-full h-full  " />
                         </div>
-                        <div className="">
-                            <img src={imgGellary3} alt="Image 3" className="object-cover w-full h-full  " />
+                        <div className="h-[140px] ">
+                            <img src={imgGellary3} alt="Image 3" className=" w-full h-full  " />
                         </div>
-                        <div className="">
-                            <img src={imgGellary4} alt="Image 4" className="object-cover w-full h-full  " />
+                        <div className=" h-[220px]">
+                            <img src={imgGellary4} alt="Image 4" className=" w-full h-full  " />
                         </div>
-                        <div className="col-span-2 ">
-                            <img src={imgGellary5} alt="Image 5" className="object-cover w-full h-full  " />
+                        <div className="col-span-2 h-[220px] ">
+                            <img src={imgGellary5} alt="Image 5" className=" w-full h-full  " />
                         </div>
-                        <div className="">
-                            <img src={imgGellary6} alt="Image 6" className="object-cover w-full h-full  " />
+                        <div className="h-[200px] ">
+                            <img src={imgGellary6} alt="Image 6" className=" w-full h-full  " />
                         </div>
-                        <div className="">
-                            <img src={imgGellary7} alt="Image 7" className="object-cover w-full h-full  " />
+                        <div className=" h-[200px] ">
+                            <img src={imgGellary7} alt="Image 7" className=" w-full h-full  " />
                         </div>
-                        <div className="">
-                            <img src={imgGellary8} alt="Image 8" className="object-cover w-full h-full  " />
+                        <div className=" h-[200px] ">
+                            <img src={imgGellary8} alt="Image 8" className=" w-full h-full  " />
                         </div>
 
                     </div>
