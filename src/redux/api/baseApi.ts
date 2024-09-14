777eaba791c76e455865fefd6620bd9e0e058853
rabby <rabby.65301@gmail.com>
2024-09-15 00:14:15 +0600

    added server url

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -5,8 +5,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
     reducerPath: "baseApi",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
-    tagTypes: ['product','categories','order'],
+    baseQuery: fetchBaseQuery({ baseUrl: `${import.meta.env.VITE_SERVER_URL}/api` }),
+    tagTypes: ['product', 'categories', 'order'],
     endpoints: () => ({})
 })
 
